af03d3c55cfe65751c5da955d41b29917bede86f
João Pedro Bastos Fernandes <bastosfjp@gmail.com>
2026-03-24 00:43:55 -0300

    Implantação do CRUD e também Criptografia

---
+++ src/infra/orm/ProdutoModel.py
@@ -0,0 +1,18 @@
+from infra import database
+from sqlalchemy import Column, VARCHAR, Float, LargeBinary, Integer
+
+# ORM
+class ProdutoDB(database.Base):
+    __tablename__ = "tb_produto"
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    nome = Column(VARCHAR(100), nullable=False)
+    descricao = Column(VARCHAR(300), nullable=False)
+    foto = Column(LargeBinary, nullable=True)
+    valor_unitario = Column(Float, nullable=False)
+
+    def __init__(self, id, nome, descricao, foto, valor_unitario):
+        self.id = id
+        self.nome = nome
+        self.descricao = descricao
+        self.foto = foto
+        self.valor_unitario = valor_unitario
